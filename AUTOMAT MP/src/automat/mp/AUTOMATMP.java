bd4c1b149208419a6ae6a4fce90da0fc2b80fa76
angeloablaza <40690315+angeloablaza@users.noreply.github.com>
2018-08-20 23:30:48 +0800

    mp

---
+++ AUTOMAT MP/src/automat/mp/AUTOMATMP.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automat.mp;
+
+/**
+ *
+ * @author Angelo Ablaza
+ */
+public class AUTOMATMP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        Model m = new Model();
+        GameFrame gf = new GameFrame();
+        gf.setVisible(true);
+      
+    }
+    
+}
